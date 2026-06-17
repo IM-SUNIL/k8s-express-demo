2ff3d46cdc909121b186f43d192c29856bb4aa13
IM-SUNIL <rajputsunil689@gmail.com>
2026-06-17 12:24:23 +0530

    add user creation endpoint with error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { Pool } = require("pg");
 
 
 const app = express();
+app.use(express.json());
 const pool = new Pool({
   host: "postgres-service",
   port: 5432,
@@ -38,6 +39,23 @@ app.get("/users", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM users");
     res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "Database error",
+    });
+  }
+});
+app.post("/users", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    const result = await pool.query(
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+      [name, email]
+    );
+
+    res.status(201).json(result.rows[0]);
   } catch (err) {
     console.error(err);
     res.status(500).json({
